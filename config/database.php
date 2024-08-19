1dcca355209b623eacd8a163eb4e65c3d1abfda2
mirza <mirzafahmi7@gmail.com>
2024-08-20 07:36:25 +0800

    fix: dockerfile remove swoole

--- config/database.php
+++ config/database.php
@@ -94,7 +94,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSL_MODE', 'required'),
+            'sslmode' => env('DB_SSL_MODE', 'require'),
         ],
 
         'sqlsrv' => [
